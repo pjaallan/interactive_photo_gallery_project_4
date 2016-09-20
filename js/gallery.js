4138c05b3de8a60cb89fadfdb4c61ee012401688
Peter Allan <peter.allan@border-control.co.uk>
2016-09-20 23:35:22 +0100

    Lightbox narrow screen

--- js/gallery.js
+++ js/gallery.js
@@ -1,14 +1,26 @@
 //variable for overlay so we can call it from where ever
 var $overlay = $('<div id="overlay"></div>');
-var $image = $("<img>");
-var $caption = $("<p></p>");
-var $arrow-l = $();
+var $imageContainer = $('<div id="imageContainer" class="clearfix"></div>');
+var $image = $('<img>');
+var $caption = $('<p></p>');
+var $next = $('<div id="next"> > </div>');
+var $previous = $('<div id="previous"> < </div>');
 
-//Add image to overlay
-$overlay.append($image);
+
+//Add imageContainer to overlay
+$overlay.append($imageContainer);
+
+//Add previous button
+$imageContainer.append($previous);
+
+//Add image to imageContainer
+$imageContainer.append($image);
+
+//Add next button
+$imageContainer.append($next);
 
 //Add caption to image
-$overlay.append($caption);
+$imageContainer.append($caption);
 
 //Add overlay div to page body
 $("body").append($overlay);
